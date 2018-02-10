30c063d80d333fa20b20d098eae0aac2b22d4c7f
John Maguire <jmaguire@mdsol.com>
2018-02-10 15:59:47 -0500

    added cask install list and yarn to brew list

---
+++ install/brew-cask.sh
@@ -0,0 +1,31 @@
+if ! is-macos -o ! is-executable brew; then
+  echo "Skipped: Homebrew-Cask"
+  return
+fi
+
+brew tap caskroom/versions
+brew tap caskroom/cask
+brew tap caskroom/fonts
+
+# Install packages
+ apps=(
+  dropbox
+  firefox
+  flux
+  iterm2
+  lastpass
+  google-chrome
+  postico
+  pulse
+  spotify
+  sublime-text
+  utorrent
+  visual-studio-code
+  vlc
+  wavebox
+ )
+
+brew cask install "${apps[@]}"
+
+# Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlimagesize webpquicklook suspicious-package qlvideo
