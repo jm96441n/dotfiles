2678948933b926220d16fad46f91c433a02b6d21
John Maguire <jmaguire@mdsol.com>
2018-02-12 18:08:57 -0500

    added bash installation

---
+++ install/bash.sh
@@ -0,0 +1,9 @@
+if ! is-macos -o ! is-executable brew; then
+  echo "Skipped: Bash 4"
+  return
+fi
+
+brew install bash
+
+grep "/usr/local/bin/bash" /private/etc/shells &>/dev/null || sudo bash -c "echo /usr/local/bin/bash >> /private/etc/shells"
+chsh -s /usr/local/bin/bash
