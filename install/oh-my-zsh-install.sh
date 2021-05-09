73a76d4b1879f8f776f68a0622d30b9d9d982f9c
jm96441n <john@johnmaguirdeveloper.com>
2021-05-09 17:46:47 -0400

    Fixing brace in oh-my-zsh isntall updating packages for installing ruby

--- install/oh-my-zsh-install.sh
+++ install/oh-my-zsh-install.sh
@@ -5,7 +5,7 @@ curl -L https://raw.githubusercontent.com/sbugzu/gruvbox-zsh/master/gruvbox.zsh-
 # zsh autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # fzf-tab
-git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab}
+git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
 # zsh syntax highlightin
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 # color ls
