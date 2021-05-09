d5858ad0d581503f1e6fba4bfa4ab47851ed0fed
jm96441n <john@johnmaguirdeveloper.com>
2021-05-09 17:55:05 -0400

    Adjust fedora installs and asdf installs

--- install/asdf_install.sh
+++ install/asdf_install.sh
@@ -1,18 +1,16 @@
 if ! is-macos -o ! is-executable brew; then
   git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0
   . $HOME/.asdf/asdf.sh
-  $RUBY_OPTS=$RUBY_CONFIGURE_OPTS 
 else
   # asdf for version management
   brew install asdf
-  $RUBY_OPTS="--with-openssl-dir=/usr/bin/openssl"
 fi
 
 
 # Ruby
 asdf plugin-add ruby
 
-RUBY_CONFIGURE_OPTS=$RUBY_OPTS asdf install ruby 2.7.2
+asdf install ruby 2.7.2
 
 asdf global ruby 2.7.2
 
