67cd59c6e92d67b2d65a28b3e43ba5ebf6a5418a
jm96441n <john@jmaguire.tech>
2021-05-09 21:26:02 -0400

    Install flatpak pacakges via function

--- install/linux/flatpak.sh
+++ install/linux/flatpak.sh
@@ -1,4 +1,10 @@
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-flatpak install flathub com.getferdi.Ferdi
-flatpak install flathub org.signal.Signal
+function install {
+  echo "Installing: ${1}..."
+  flatpak install flathub $1 -y
+}
+
+install com.getferdi.Ferdi
+install org.signal.Signal
+install com.spotify.Client
