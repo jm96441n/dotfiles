4f8bec615e2f4bb761faa18b78801ccde46adc56
jm96441n <john@jmaguire.tech>
2021-05-09 21:09:24 -0400

    Install ferdi and signal with flatpak

---
+++ install/linux/flatpak.sh
@@ -0,0 +1,5 @@
+flatpak remote-add --if-not-exists gnome-nightly https://sdk.gnome.org/gnome-nightly.flatpakrepo
+flatpak remote-add --if-not-exists gnome-apps-nightly https://sdk.gnome.org/gnome-apps-nightly.flatpakrepo
+
+flatpak install flathub com.getferdi.Ferdi
+flatpak install flathub org.signal.Signal
