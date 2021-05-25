93545176e383680c53aabe1828c83daa7ed90fe4
jm96441n <john@jmaguire.tech>
2021-05-24 22:13:22 -0400

    Fedora changes

--- install/linux/fonts.sh
+++ install/linux/fonts.sh
@@ -1,8 +1,8 @@
-mkdir -p ~/.local/share/fonts
-cd ~/.local/share/fonts 
-
-	
-curl -fLo "Hack Nerd Font Complete.otf" \
-    https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Hack/Regular/complete/Hack%20Regular%20Nerd%20Font%20Complete.ttf
-
+git clone --depth=1 https://github.com/ryanoasis/nerd-fonts ~/.nerd-fonts
+cd .nerd-fonts
+sudo .install-sh
 cd ~
+
+sudo ln -sfv $HOME/.dotfiles/system/fonts/icomoon-feather.ttf /usr/share/fonts
+sudo ln -sfv $HOME/.dotfiles/system/fonts/icomoon-feather.ttf_ /usr/share/fonts
+sudo ln -sfv $HOME/.dotfiles/system/fonts/siji.pcf /usr/share/fonts
