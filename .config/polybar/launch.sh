488e55d32dd3491b58722de50b3f4cef2797867b
john.maguire <john.maguire@cbinsights.com>
2021-12-13 10:22:00 -0500

    Updated polybar to launch correctly, set background correctly from i3 config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,7 +6,7 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $USER -x polybar >/dev/null; do sleep 1; done
 
 # Launch fullbar
 polybar fullbar &
