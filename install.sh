73e4744dd55bff2ae6ee3f798678fdf57b6c00e6
john.maguire <john.maguire@cbinsights.com>
2021-12-21 15:17:15 -0500

    Added everforest to everything

--- install.sh
+++ install.sh
@@ -65,6 +65,16 @@ if [ -d "$DOTFILES_EXTRA_DIR" -a -f "$DOTFILES_EXTRA_DIR/install.sh" ]; then
     . "$DOTFILES_EXTRA_DIR/install.sh"
 fi
 
+# add theme for bat
+mkdir -p "$(bat --config-dir)/themes"
+cd "$(bat --config-dir)/themes"
+
+# Download a theme in '.tmTheme' format, for example:
+git clone https://github.com/mhanberg/forest-night-textmate.git
+
+# Update the binary cache
+bat cache --build
+
 echo "Installing zsh"
 
 . "$DOTFILES_DIR/install/zsh_install.sh"
