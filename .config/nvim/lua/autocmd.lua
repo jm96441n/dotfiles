ebea3f0e27fcef07c307a70188df4cffadc41936
john.maguire <john.maguire@cbinsights.com>
2021-12-21 18:15:48 -0500

    Changes to polybar and autoformat noevim

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -44,7 +44,7 @@ local autoCommands = {
         {"BufWritePre", "*", [[lua require("utils").preserve('%s/\\s\\+$//ge')]]}
     };
     format = {
-        {"BufWritePre", "*", "undojoin | Neoformat"}
+        {"BufWritePre", "*", "Neoformat"}
     };
 
 }
