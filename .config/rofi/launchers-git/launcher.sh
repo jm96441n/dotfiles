640b5f3e8b25ba648b0233eb1ca42ab6882d93f2
jm96441n <john@johnmaguirdeveloper.com>
2021-12-22 09:05:42 -0500

    Added rofi launcher theme, modified kitty config and set polybar dpi for better viewing on fedora

--- .config/rofi/launchers-git/launcher.sh
+++ .config/rofi/launchers-git/launcher.sh
@@ -2,4 +2,4 @@
 
 style="blurry_full"
 
-zsh -c "rofi -show drun -theme $HOME/.config/rofi/themes/colorschemes/gruvbox.rasi"
+zsh -c "rofi -show drun -theme $HOME/.config/rofi/themes/everforest-dark.rasi"
