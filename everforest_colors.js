29a1b0008d30246520c4a40ab176e517ff7d8d8f
jm96441n <john@jmaguire.tech>
2021-12-26 15:11:48 -0500

    Added themer info for everforest

---
+++ everforest_colors.js
@@ -0,0 +1,18 @@
+// This file can be used with the themer CLI, see https://github.com/themerdev/themer
+
+module.exports.colors = {
+  "dark": {
+    "shade0": "#2B3339",
+    "shade7": "#D3C6AA",
+    "accent0": "#E67E90",
+    "accent1": "#E69875",
+    "accent2": "#DBBC7F",
+    "accent3": "#A7C080",
+    "accent4": "#83C092",
+    "accent5": "#7FBBB3",
+    "accent6": "#83C092",
+    "accent7": "#D699B6"
+  }
+};
+
+// Your theme's URL: https://themer.dev/?colors.dark.shade0=%232b3339&colors.dark.shade7=%23d3c6aa&colors.dark.accent0=%23e67e90&colors.dark.accent1=%23e69875&colors.dark.accent2=%23dbbc7f&colors.dark.accent3=%23a7c080&colors.dark.accent4=%2383c092&colors.dark.accent5=%237fbbb3&colors.dark.accent7=%23d699b6&colors.dark.accent6=%2383c092
