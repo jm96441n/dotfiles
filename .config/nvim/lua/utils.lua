aef784204f6f3613ae855ca7d703ccc5a928b515
john.maguire <john.maguire@cbinsights.com>
2022-04-25 09:34:55 -0400

    formatting

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -60,12 +60,12 @@ function M.preserve(arguments)
     local unpack = table.unpack or unpack
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
     vim.api.nvim_command(arguments)
-	local lastline = vim.fn.line('$')
-    -- vim.fn.winrestview(original_cursor)
-	if line > lastline then
-		line = lastline
-	end
-	api.nvim_win_set_cursor({0}, {line , col})
+    local lastline = vim.fn.line('$')
+-- vim.fn.winrestview(original_cursor)
+    if line > lastline then
+            line = lastline
+    end
+    --api.nvim_win_set_cursor({0}, {line , col})
 end
 
 function M.AgSearch()
