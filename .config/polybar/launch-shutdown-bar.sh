52cd567c9e05d7758e6132c3c60f984be5db2995
john.maguire <john.maguire@cbinsights.com>
2022-05-17 10:19:45 -0400

    Update treesitter and polybar for different inputs

--- .config/polybar/launch-shutdown-bar.sh
+++ .config/polybar/launch-shutdown-bar.sh
@@ -12,6 +12,7 @@ launch_bar() {
         polybar -q power -c "$DIR"/config &
     else
         polybar-msg -p $(xprop -name "polybar-power_DP-2" _NET_WM_PID | cut -d ' ' -f 3) cmd toggle
+        polybar-msg -p $(xprop -name "polybar-power_HDMI-1" _NET_WM_PID | cut -d ' ' -f 3) cmd toggle
     fi
 }
 
