5ab895a202df4acb0cc40edd8f7619a30cc321f7
john.maguire <john.maguire@cbinsights.com>
2022-09-01 10:25:41 -0400

    setting up local reviews

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -9,44 +9,46 @@ g.mapleader = " "
 -- CTRL-N and CTRL-P will be automatically bound to next-history and
 -- previous-history instead of down and up. If you don't like the change,
 -- explicitly bind the keys to down and up in your $FZF_DEFAULT_OPTS.
-g.fzf_history_dir = '~/.local/share/fzf-history'
+g.fzf_history_dir = "~/.local/share/fzf-history"
 
-g.everforest_background = 'hard'
+g.everforest_background = "hard"
 
-opt.termguicolors=true
-opt.mouse = 'c' -- turn off mouse
-opt.clipboard='unnamedplus' -- always default to system clipboard
-opt.autoread=true -- autoread files when they change
+g.indentLine_setConceal = 1
+
+opt.termguicolors = true
+opt.mouse = "c" -- turn off mouse
+opt.clipboard = "unnamedplus" -- always default to system clipboard
+opt.autoread = true -- autoread files when they change
 opt.number = true -- line numbers
 opt.wrap = false -- turn off word wrap
-opt.background='dark' -- highlight Normal ctermbg=None
-opt.autoindent=true -- turn on autoindent
-opt.timeout=true -- turn on timeout
-opt.timeoutlen=1000 -- set timeout to 1000
-opt.smarttab=true -- turn on smarttab
-opt.incsearch=true -- turn on incremental search
-opt.inccommand='nosplit'-- turn on incremental substitution
-opt.laststatus=2 -- always turn on status bar
-opt.ruler=true -- show ruler on page
-opt.relativenumber=true -- set relative line numbers
-opt.display=(opt.display + 'lastline') -- include as much as possible of the line on the screen
-opt.autoread=true -- automatically detect changes to files
-opt.sessionoptions=(opt.sessionoptions - 'options')
-opt.viewoptions=(opt.viewoptions - 'option')
+opt.background = "dark" -- highlight Normal ctermbg=None
+opt.autoindent = true -- turn on autoindent
+opt.timeout = true -- turn on timeout
+opt.timeoutlen = 1000 -- set timeout to 1000
+opt.smarttab = true -- turn on smarttab
+opt.incsearch = true -- turn on incremental search
+opt.inccommand = "nosplit" -- turn on incremental substitution
+opt.laststatus = 2 -- always turn on status bar
+opt.ruler = true -- show ruler on page
+opt.relativenumber = true -- set relative line numbers
+opt.display = (opt.display + "lastline") -- include as much as possible of the line on the screen
+opt.autoread = true -- automatically detect changes to files
+opt.sessionoptions = (opt.sessionoptions - "options")
+opt.viewoptions = (opt.viewoptions - "option")
 
-opt.foldmethod='expr'
-opt.foldexpr='nvim_treesitter#foldexpr()'
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
 
 opt.list = true
 opt.listchars:append("eol:↴")
 
 -- tabs to 4 spaces
-opt.expandtab=true
-opt.shiftwidth=4
-opt.softtabstop=4
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.softtabstop = 4
 
 -- set bash like file autocompletion
-opt.wildmode='longest,list,full'
-opt.wildmenu=true
+opt.wildmode = "longest,list,full"
+opt.wildmenu = true
 
-cmd('filetype plugin indent on')
+cmd("filetype plugin indent on")
