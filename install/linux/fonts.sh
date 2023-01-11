e3023e3ffc3a87b357a400977ff0059dfcd028a9
jm96441n <john@jmaguire.tech>
2023-01-11 11:12:36 -0500

    cleaning up apt setup

--- install/linux/fonts.sh
+++ install/linux/fonts.sh
@@ -2,10 +2,7 @@ if [ ! -d ~/.nerd-fonts ]; then
 	git clone --depth=1 https://github.com/ryanoasis/nerd-fonts ~/.nerd-fonts
 fi
 
-cd ~/.nerd-fonts
-echo $PWD
-./install-sh
-cd ~
+~/.nerd-fonts/install-sh
 
 sudo ln -sfv $HOME/.dotfiles/system/fonts/icomoon-feather.ttf /usr/share/fonts
 sudo ln -sfv $HOME/.dotfiles/system/fonts/icomoon-feather.ttf_ /usr/share/fonts
