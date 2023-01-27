4d603c4b44da300ee9157230bad6cc057dc6cd39
jm96441n <john@jmaguire.tech>
2023-01-27 13:57:57 -0500

    fix shutdown bar to listenf or correct input name

--- .config/polybar/launch-shutdown-bar.sh
+++ .config/polybar/launch-shutdown-bar.sh
@@ -8,12 +8,12 @@ MFILE="$DIR/.module"
 
 ## Launch Polybar with selected style
 launch_bar() {
-  if [[ $(pidof polybar) && $(pidof polybar | wc -w) -lt 2 ]]; then
-    polybar -q power -c "$DIR"/config &
-  else
-    polybar-msg -p $(xprop -name "polybar-power_DP-2" _NET_WM_PID | cut -d ' ' -f 3) cmd toggle
-    polybar-msg -p $(xprop -name "polybar-power_HDMI-1" _NET_WM_PID | cut -d ' ' -f 3) cmd toggle
-  fi
+    if [[ $(pidof polybar) && $(pidof polybar | wc -w) -lt 2 ]]; then
+        polybar -q power -c "$DIR"/config &
+    else
+        polybar-msg -p $(xprop -name "polybar-power_DP-2-1" _NET_WM_PID | cut -d ' ' -f 3) cmd toggle
+        polybar-msg -p $(xprop -name "polybar-power_HDMI-1" _NET_WM_PID | cut -d ' ' -f 3) cmd toggle
+    fi
 }
 
 launch_bar
