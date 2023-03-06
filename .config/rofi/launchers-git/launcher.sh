3d4188f14b68ed6fceeef36c35961ea9043ef9cc
jm96441n <john@jmaguire.tech>
2023-03-06 14:14:59 -0500

    fix start up of rofi

--- .config/rofi/launchers-git/launcher.sh
+++ .config/rofi/launchers-git/launcher.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-zsh -c "source $HOME/.dotfiles/system/.private_env && rofi -show github -theme $HOME/.config/rofi/themes/everforest-dark.rasi -log $HOME/rofi.log"
+zsh -c "source $HOME/.dotfiles/system/.private_env && rofi -show drun -theme $HOME/.config/rofi/themes/everforest-dark.rasi"
